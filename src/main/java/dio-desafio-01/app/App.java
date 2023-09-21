adca49d75bf85a45b27f3cd2bed6daa3669991d4
Rafael da Conceição <mnraffaell@gmail.com>
2023-09-20 22:59:11 -0300

    Initial commit

---
+++ src/main/java/dio-desafio-01/app/App.java
@@ -0,0 +1,13 @@
+package dio-desafio-01.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
